c8b42ea6fecaa6ef9a7519ea8be2f7bdd2a97628
carloshakim00 <carloshakim00@gmail.com>
2024-03-12 22:58:24 +0700

    feat:error handler

---
+++ Server/app.js
@@ -0,0 +1,29 @@
+if(process.env.NODE_ENV !== 'production'){
+
+    require("dotenv").config();
+  }
+  
+  
+  const express = require("express");
+  const errorHandler = require("./middlewares/errorHandler");
+
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+  const router = require("./routes")
+  const cors = require("cors")
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(router)
+  app.use(cors())
+  //errorHandler
+  app.use(errorHandler)
+  
+  app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+  });
+  
+  module.exports = app
+  
+  
+  
