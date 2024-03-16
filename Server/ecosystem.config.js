0c60b780a7ee28ee34ce82e5bbfaf0810ff4f446
root <root@ip-172-31-25-255.ap-southeast-1.compute.internal>
2024-03-16 08:24:44 +0000

    fix

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -1,22 +1,38 @@
-module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+	    apps: [
+		            {
+				                name: "MedShop",
+				                script: "./bin/www",
+				                env:{
+							                NODE_ENV: "production",
+							                PORT: 80,
+							                JWT_SECRET:"rahasia",
+							                cloud_name:"dpmwni4tm",
+							                api_key:134972379139894,
+							                api_secret:"xjCSBxEh5RFiwnZ0UQGLfL4W-as",
+							                DATABASE_URL:"postgres://postgres.hhkhifaorqrbqjtoxnoo:5E63VM6HiORS1VCy@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
+							            },
+				            },
+		        ],
 };
