e8f2c41e26ccdb49407ed7db0d45c9f64af70e23
root <root@ip-172-31-25-255.ap-southeast-1.compute.internal>
2024-03-16 07:57:54 +0000

    test

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -1,16 +1,16 @@
- module.exports = {
-	apps: [
-		{
-		name: "medshop",
-		script: "./bin/www",
-		env:{
+module.exports = {
+	    apps: [
+		            {
+				                name: "MedShop",
+				                script: "./bin/www",
+				                env:{
 							                NODE_ENV: "production",
-							                PORT: 80,
+							                PORT:80,
 							                JWT_SECRET:"rahasia",
 							                cloud_name:"dpmwni4tm",
 							                api_key:134972379139894,
 							                api_secret:"xjCSBxEh5RFiwnZ0UQGLfL4W-as",
-							                DATABASE_URL:"postgres://postgres.jrmpziprwxhdohgikgkl:rt7VCXCE7IYpDra8@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
+							                DATABASE_URL:"postgres://postgres.hhkhifaorqrbqjtoxnoo:5E63VM6HiORS1VCy@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
 							            },
 				            },
 		        ],
